eb71295df4452315e4d6fbf1c19ce8d78fbe9d06
kakunindima <36222028+kakunindima@users.noreply.github.com>
2020-06-29 17:07:34 +0300

    Update init.sh

--- less33/script/init.sh
+++ less33/script/init.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-cat > /tmp/setupCluster.js <<EOF
+cat > /tmp/init.js <<EOF
 var dbPass = "mysql"
 var clusterName = "TestCluster"
 try {
-  shell.connect('root@node1:3306', dbPass)
+  shell.connect('root@node1_1:3306', dbPass)
   var cluster = dba.createCluster(clusterName);
-  cluster.addInstance({user: "root", host: "node2", password: dbPass});
-  cluster.addInstance({user: "root", host: "node3", password: dbPass});
+  cluster.addInstance({user: "root", host: "node2_1", password: dbPass});
+  cluster.addInstance({user: "root", host: "node3_1", password: dbPass});
 } catch(e) {
   print('\nThe InnoDB cluster could not be created.\n\nError: ' + e.message + '\n');
 }
 EOF
 
 until (
-    mysqlsh --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} --port=${MYSQL_PORT} --interactive --file=/tmp/setupCluster.js
+    mysqlsh --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} --port=${MYSQL_PORT} --interactive --file=/tmp/init.js
 )
 do
     sleep 5

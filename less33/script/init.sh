d755ad56eef08bf818256bbfb99bcb3508eaf230
kakunindima <kakunindima@gmail.com>
2020-06-29 15:37:29 +0300

    less33 add

---
+++ less33/script/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat > /tmp/setupCluster.js <<EOF
+var dbPass = "mysql"
+var clusterName = "TestCluster"
+try {
+  shell.connect('root@node1:3306', dbPass)
+  var cluster = dba.createCluster(clusterName);
+  cluster.addInstance({user: "root", host: "node2", password: dbPass});
+  cluster.addInstance({user: "root", host: "node3", password: dbPass});
+} catch(e) {
+  print('\nThe InnoDB cluster could not be created.\n\nError: ' + e.message + '\n');
+}
+EOF
+
+until (
+    mysqlsh --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${MYSQL_HOST} --port=${MYSQL_PORT} --interactive --file=/tmp/setupCluster.js
+)
+do
+    sleep 5
+done
